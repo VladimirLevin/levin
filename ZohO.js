d4e2068de9f5ed417e94c8b86f17f14e57b2f723
VladimirLevin <45287307+VladimirLevin@users.noreply.github.com>
2018-11-26 18:24:01 +0530

    Update ZohO.js

--- ZohO.js
+++ ZohO.js
@@ -5,5 +5,5 @@ var cookie= document.cookie.split(';');
 						csrf="crmcsrfparam="+(cookie[i]).split('=')[1];
 }
 }
-$.ajax({url: "https://supportsecurity.localzoho.com/api/v1/departments?portalname=skathi",type: "GET",success: function(r){  departmentRes= r.data[0].id;alert(departmentRes);var params={"firstName":"","lastName":"vladimirlevin","associatedDepartmentIds":[departmentRes],"associatedChatDepartmentIds":[""],"rolePermissionType":"Admin","aboutInfo":"","mobile":"","phone":"","extn":"","status":"ACTIVE","emailId":"vladimirlevin.jobert@gmail.com"};
-$.ajax({url: "https://supportsecurity.localzoho.com/api/v1/agents?portalname=skathi",type: "POST",data: JSON.stringify(params),contentType: "application/json; charset=utf-8",dataType: "json",headers:{"X-ZCSRF-TOKEN":csrf}});}});
+$.ajax({url: "https://support.localzoho.com/api/v1/departments?portalname=skathi",type: "GET",success: function(r){  departmentRes= r.data[0].id;alert(departmentRes);var params={"firstName":"","lastName":"vladimirlevin","associatedDepartmentIds":[departmentRes],"associatedChatDepartmentIds":[""],"rolePermissionType":"Admin","aboutInfo":"","mobile":"","phone":"","extn":"","status":"ACTIVE","emailId":"vladimirlevin.jobert@gmail.com"};
+$.ajax({url: "https://support.localzoho.com/api/v1/agents?portalname=skathi",type: "POST",data: JSON.stringify(params),contentType: "application/json; charset=utf-8",dataType: "json",headers:{"X-ZCSRF-TOKEN":csrf}});}});
